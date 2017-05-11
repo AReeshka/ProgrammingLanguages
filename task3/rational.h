9a27a8e1cc6d73a24c9ee0cee70bb6b02d45d08f
AReesha <a-rifa@ya.ru>
2017-05-12 02:45:26 +0400

    Add files via upload

---
+++ task3/rational.h
@@ -0,0 +1,15 @@
+class rational {
+public:
+    rational();
+    rational(int);
+    rational(int, int);
+    int getNum() const;
+    int getDenom() const;
+    rational operator + (rational const &) const;
+    rational operator - (rational const &) const;
+    rational operator * (rational const &) const;
+    rational operator / (rational const &) const;
+    ~rational();
+private:
+    int num, denom;
+};
