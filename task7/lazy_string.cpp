c7470aff0b83ed45b2aaad74894d6e2b18432df5
AReesha <a-rifa@ya.ru>
2017-05-13 03:04:44 +0400

    Add files via upload

---
+++ task7/lazy_string.cpp
@@ -0,0 +1,91 @@
+#include "lazy_string.h"
+#include <string>
+
+void lazy_string::ref_counter() {
+    if (smart_pointer->links == 1) return;
+    shared *now = new shared((smart_pointer->data).substr(index, lengh));
+    (smart_pointer->links)--;
+    smart_pointer = now;
+    index = 0;
+}
+
+lazy_string::lazy_string() {
+    smart_pointer = new shared();
+    index = lengh = 0;
+}
+
+lazy_string::~lazy_string() {
+    (smart_pointer->links)--;
+    if (smart_pointer->links == 0)
+        delete smart_pointer;
+}
+
+lazy_string::lazy_string(const std::string &q) {
+    smart_pointer = new shared(q);
+    index = 0;
+    lengh = q.size();
+}
+
+lazy_string::lazy_string(const lazy_string &q) {
+    smart_pointer = q.smart_pointer;
+    (smart_pointer->links)++;
+    index = q.index;
+    lengh = q.lengh;
+}
+
+
+char lazy_string::at(size_t ind) const {
+    return smart_pointer->data[index + ind];
+}
+
+size_t lazy_string::size() const {
+    return lengh;
+}
+
+size_t lazy_string::length() const {
+    return lengh;
+}
+
+
+lazy_string::help lazy_string::operator[](size_t pos) {
+    return help(this, pos);
+}
+
+char lazy_string::operator[](size_t pos) const {
+    return smart_pointer->data[index + pos];
+}
+
+
+std::ostream &operator<<(std::ostream &out, lazy_string &q) {
+    q.mut.lock();
+    for (size_t i = q.index; i < q.index + q.lengh; i++) {
+        out << (q.smart_pointer->data[i]);
+    }
+    q.mut.unlock();
+    return out;
+}
+
+std::istream &operator>>(std::istream &in, lazy_string &q) {
+    q.mut.lock();
+    q.ref_counter();
+    in >> q.smart_pointer->data;
+    q.index = 0;
+    q.lengh = q.smart_pointer->data.length();
+    q.mut.unlock();
+    return in;
+}
+
+lazy_string lazy_string::substr(size_t begin, size_t len) {
+    mut.lock();
+    lazy_string *tmp = new lazy_string();
+    tmp->smart_pointer = smart_pointer;
+    tmp->index = index + begin;
+    tmp->lengh = len;
+    (smart_pointer->links)++;
+    mut.unlock();
+    return *tmp;
+}
+
+int main() {
+    return 0;
+}
---
+++ task7/lazy_string.h
@@ -0,0 +1,80 @@
+#ifndef LAZY_STRING_H
+#define LAZY_STRING_H
+
+
+#include <iostream>
+#include <string>
+
+class lazy_string {
+private:
+    struct shared {
+        std::string data;
+        int links;
+
+        shared() {
+            data = std::string();
+            links = 1;
+        }
+
+        shared(const std::string &s) {
+            data = s;
+            links = 1;
+        }
+    };
+
+    struct help {
+    private:
+        lazy_string *str;
+        int ind;
+
+    public:
+        help(lazy_string *s, int pos) {
+            str = s;
+            ind = pos;
+        }
+
+        help &operator=(const char c) {
+            str->ref_counter();
+            str->smart_pointer->data[ind] = c;
+            return *this;
+        }
+
+        operator char() const {
+            return str->smart_pointer->data[ind];
+        }
+    };
+
+    shared *smart_pointer;
+    int lengh, index;
+    std::mutex mut;
+    void ref_counter();
+
+public:
+    lazy_string();
+
+    ~lazy_string();
+
+    lazy_string(const std::string &);
+
+    lazy_string(const lazy_string &);
+
+    char at(size_t) const;
+
+    size_t size() const;
+
+    size_t length() const;
+
+
+    help operator[](size_t);
+
+    char operator[](size_t) const;
+
+
+    friend std::ostream &operator<<(std::ostream &, lazy_string &);
+
+    friend std::istream &operator>>(std::istream &, lazy_string &);
+
+    lazy_string substr(size_t, size_t);
+};
+
+#endif
